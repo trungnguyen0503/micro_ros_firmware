85c984ed47ed71453920432e59b6237d2f9dfbcd
trungnguyen0503 <nqt2003i@gmail.com>
2025-07-17 17:19:43 +0700

    Added teleop node structure

--- Core/Src/project/task/subscribe.c
+++ Core/Src/project/task/subscribe.c
@@ -12,7 +12,6 @@ void StartSubscribeTask(void *argument) {
     while (1) {
         const uint32_t interval_ms = 50;
         const uint32_t interval_ns = interval_ms * 1000;
-
         rclc_executor_spin_some(&Ros_Teleop_exec, interval_ns);
 
         osDelay(interval_ms);
