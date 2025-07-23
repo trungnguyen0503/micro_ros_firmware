5a350e007eb0ad205600b05560f453cc186967f8
trungnguyen0503 <nqt2003i@gmail.com>
2025-07-23 22:02:30 +0700

    Cleaned up includes

--- Core/Src/project/task/init.c
+++ Core/Src/project/task/init.c
@@ -4,12 +4,8 @@
 #include "project/ros/imu_node.h"
 #include "project/ros/teleop_node.h"
 #include "project/sensor/motor_encoder.h"
-#include "project/uros/utility.h"
-
-#include "rmw_microros/rmw_microros.h"
 
 #include "cmsis_os2.h"
-#include "usart.h"
 
 void StartInitTask(void *argument) {
     UNUSED(argument);
