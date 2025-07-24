431c3055d5a35d3963e93886b2230e910e02f457
trungnguyen0503 <nqt2003i@gmail.com>
2025-07-24 20:55:45 +0700

    Added function to calculate time diff

--- Core/Inc/project/utility.h
+++ Core/Inc/project/utility.h
@@ -5,5 +5,10 @@
 
 // Convert RTOS tick count into ROS' time message
 builtin_interfaces__msg__Time Utility_GetRosTimeStamp();
+// Calculate the time difference between 2 ROS' time message in nanoseconds (t1 - t2)
+int64_t Utility_GetRosTimeDiffNs(
+    builtin_interfaces__msg__Time t1,
+    builtin_interfaces__msg__Time t2
+);
 
 #endif
--- Core/Src/project/utility.c
+++ Core/Src/project/utility.c
@@ -1,5 +1,6 @@
 #include "project/utility.h"
 
+#include "builtin_interfaces/msg/detail/time__struct.h"
 #include "cmsis_os2.h"
 #include "rmw_microros/time_sync.h"
 
@@ -9,4 +10,13 @@ builtin_interfaces__msg__Time Utility_GetRosTimeStamp() {
         .sec = (int32_t)(ns / 1000000000),
         .nanosec = (uint32_t)(ns % 1000000000),
     };
+}
+
+int64_t Utility_GetRosTimeDiffNs(
+    const builtin_interfaces__msg__Time t1,
+    const builtin_interfaces__msg__Time t2
+) {
+    const int64_t t1_ns = (int64_t)t1.sec * 1000000000 + t1.nanosec;
+    const int64_t t2_ns = (int64_t)t2.sec * 2000000000 + t2.nanosec;
+    return t1_ns - t2_ns;
 }
