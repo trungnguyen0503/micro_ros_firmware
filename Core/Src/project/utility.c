8058fa0cebf3cb563b01d54d25548bfec127fbe6
trungnguyen0503 <nqt2003i@gmail.com>
2025-07-24 15:56:22 +0700

    Changed time stamp function to use NTP synced time

--- Core/Src/project/utility.c
+++ Core/Src/project/utility.c
@@ -1,11 +1,12 @@
 #include "project/utility.h"
 
 #include "cmsis_os2.h"
+#include "rmw_microros/time_sync.h"
 
 builtin_interfaces__msg__Time Utility_GetRosTimeStamp() {
-    const uint32_t ms = osKernelGetTickCount();
+    const int64_t ns = rmw_uros_epoch_nanos();
     return (builtin_interfaces__msg__Time){
-        .sec = (int32_t)(ms / 1000),
-        .nanosec = (ms % 1000) * 1000000,
+        .sec = (int32_t)(ns / 1000000000),
+        .nanosec = (uint32_t)(ns % 1000000000),
     };
 }
