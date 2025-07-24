88dcdfb6ca37c04866ceb6fa9e2244c5c27882a2
trungnguyen0503 <nqt2003i@gmail.com>
2025-07-24 16:16:04 +0700

    Added time sync indicator led

--- Core/Src/project/task/time_sync.c
+++ Core/Src/project/task/time_sync.c
@@ -1,11 +1,19 @@
 #include "rmw_microros/time_sync.h"
 
 #include "cmsis_os2.h"
+#include "gpio.h"
 
 void StartTimeSyncTask(void *argument) {
     (void)argument;
+
     while (1) {
+        HAL_GPIO_WritePin(
+            GPIOC, GPIO_PIN_2,
+            rmw_uros_epoch_synchronized() ? GPIO_PIN_SET : GPIO_PIN_RESET
+        );
+
         osDelay(60000);
+
         const rmw_ret_t ret = rmw_uros_sync_session(100);
         if (ret != RMW_RET_OK) {
             // TODO:
