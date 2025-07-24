d8b9cfbf0c89505dfa0e90fb178d19abe4efc353
trungnguyen0503 <nqt2003i@gmail.com>
2025-07-24 17:46:59 +0700

    Fixed teleop task interval

--- Core/Src/project/task/teleop.c
+++ Core/Src/project/task/teleop.c
@@ -7,9 +7,8 @@ void StartTeleopTask(void *argument) {
 
     while (1) {
         const uint32_t interval_ms = 50;
-        const uint32_t interval_ns = interval_ms * 1000;
+        const uint32_t interval_ns = interval_ms * 1000000;
         rclc_executor_spin_some(Ros_TeleopNode_GetExec(), interval_ns);
-
         osDelay(interval_ms);
     }
 }
