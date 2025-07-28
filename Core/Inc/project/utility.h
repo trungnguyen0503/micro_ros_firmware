0dc4123b6d25965a723317942cd32ec848c34e65
trungnguyen0503 <nqt2003i@gmail.com>
2025-07-28 21:42:42 +0700

    Added utility to make ros string

--- Core/Inc/project/utility.h
+++ Core/Inc/project/utility.h
@@ -2,9 +2,18 @@
 #define PROJECT_UTILITY_H
 
 #include "builtin_interfaces/msg/time.h"
+#include "rosidl_runtime_c/string.h"
+
+#define Utility_MakeStaticRosCString(literal_string) \
+    ((rosidl_runtime_c__String){ \
+        .capacity = sizeof(literal_string), \
+        .size = sizeof(literal_string) - 1, \
+        .data = literal_string, \
+    })
 
 // Convert RTOS tick count into ROS' time message
 builtin_interfaces__msg__Time Utility_GetRosTimeStamp();
+
 // Calculate the time difference between 2 ROS' time message in nanoseconds (t1 - t2)
 int64_t Utility_GetRosTimeDiffNs(
     builtin_interfaces__msg__Time t1,
