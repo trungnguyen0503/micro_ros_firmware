72ac1eca89ed522fba8d6c7eb117d99239b970a0
trungnguyen0503 <nqt2003i@gmail.com>
2025-07-31 14:36:14 +0700

    Added logging and fail condition for initialization

--- Core/Src/project/ros/global.c
+++ Core/Src/project/ros/global.c
@@ -1,5 +1,6 @@
 #include "project/ros/global.h"
 #include "project/uros/utility.h"
+#include "project/utility.h"
 
 #include "rclc/rclc.h"
 #include "rmw_microros/rmw_microros.h"
@@ -21,7 +22,9 @@ void Ros_Init() {
             Uros_UartDmaTransport_Read
         );
         if (ret != RMW_RET_OK) {
-            // TODO:
+            Utility_Log(Utility_LogFatal, "Failed to initialize ros transport");
+            while (1) {
+            }
         }
     }
     // Set Uros custom allocator
@@ -31,21 +34,25 @@ void Ros_Init() {
         Ros_allocator.reallocate = Uros_Reallocate;
         Ros_allocator.zero_allocate = Uros_ZeroAllocate;
         if (!rcutils_set_default_allocator(&Ros_allocator)) {
-            // TODO:
+            Utility_Log(Utility_LogFatal, "Failed to initialize allocator for ros");
+            while (1) {
+            }
         }
     }
     // Set support struct
     {
         const rcl_ret_t ret = rclc_support_init(&Ros_support, 0, NULL, &Ros_allocator);
         if (ret != RCL_RET_OK) {
-            // TODO:
+            Utility_Log(Utility_LogFatal, "Failed to initialize support struct for ros");
+            while (1) {
+            }
         }
     }
     // Sync time with agent using NTP
     {
         const rmw_ret_t ret = rmw_uros_sync_session(1000);
         if (ret != RMW_RET_OK) {
-            // TODO:
+            Utility_Log(Utility_LogWarning, "Failed to sync time with agent");
         }
     }
 }
