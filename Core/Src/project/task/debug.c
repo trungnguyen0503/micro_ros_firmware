e67ae7a8ff0868645c411f2bb8b51e44a2c1285e
trungnguyen0503 <nqt2003i@gmail.com>
2025-08-07 21:28:00 +0700

    Removed RPM debug

--- Core/Src/project/task/debug.c
+++ Core/Src/project/task/debug.c
@@ -4,19 +4,15 @@
 
 #include "cmsis_os.h"
 
-static size_t DEBUG_min_heap_free = 0;
+static size_t DEBUG_freertos_min_heap_free = 0;
 static size_t DEBUG_microros_min_heap_free = 0;
-static double DEBUG_left_rpm = 0;
-static double DEBUG_right_rpm = 0;
 
 void StartDebugTask(void *argument) {
     UNUSED(argument);
 
     while (1) {
-        DEBUG_min_heap_free = xPortGetMinimumEverFreeHeapSize();
+        DEBUG_freertos_min_heap_free = xPortGetMinimumEverFreeHeapSize();
         DEBUG_microros_min_heap_free = Uros_GetMinFreeHeapSize();
-        DEBUG_left_rpm = Sensor_MotorEncoder_GetLeftAngularVel() * 60.0 / (2.0 * M_PI);
-        DEBUG_right_rpm = Sensor_MotorEncoder_GetRightAngularVel() * 60.0 / (2.0 * M_PI);
         HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_1);
 
         osDelay(1000);
