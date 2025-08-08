367131d986b0876ef9325aca49a2d0980ead2c23
trungnguyen0503 <nqt2003i@gmail.com>
2025-08-08 23:43:16 +0700

    Fixed status led. Adjusted sync interval

--- Core/Src/project/task/time_sync.c
+++ Core/Src/project/task/time_sync.c
@@ -1,22 +1,40 @@
+#include "project/task/init.h"
+#include "project/utility.h"
+
 #include "rmw_microros/time_sync.h"
 
 #include "cmsis_os2.h"
 #include "gpio.h"
 
+#define STATUS_LED_GPIO GPIOC
+#define STATUS_LED_PIN GPIO_PIN_2
+
+static void SetStatusLed(bool on);
+
 void StartTimeSyncTask(void *argument) {
     (void)argument;
+    Task_Init_WaitUntilDone();
+    SetStatusLed(rmw_uros_epoch_synchronized());
 
     while (1) {
-        HAL_GPIO_WritePin(
-            GPIOC, GPIO_PIN_2,
-            rmw_uros_epoch_synchronized() ? GPIO_PIN_SET : GPIO_PIN_RESET
-        );
+        osDelay(20000);
 
-        osDelay(60000);
-
-        const rmw_ret_t ret = rmw_uros_sync_session(100);
-        if (ret != RMW_RET_OK) {
-            // TODO:
+        const rmw_ret_t ret = rmw_uros_sync_session(1000);
+        if (ret == RMW_RET_OK) {
+            SetStatusLed(true);
+        } else {
+            SetStatusLed(false);
+            Utility_Log(
+                Utility_LogWarning,
+                "Failed to sync time with the agent (code %d)", (int)ret
+            );
         }
     }
+}
+
+static void SetStatusLed(const bool on) {
+    HAL_GPIO_WritePin(
+        STATUS_LED_GPIO, STATUS_LED_PIN,
+        on ? GPIO_PIN_RESET : GPIO_PIN_SET
+    );
 }
